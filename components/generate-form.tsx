9b205372d81ac6392debefb88bdb7ab9e4c2f9f5
Aldiyar Serikov <ldr.stlt@gmail.com>
2023-06-21 10:16:00 +0600

    fix: add toaster component that cli forgot

--- components/generate-form.tsx
+++ components/generate-form.tsx
@@ -65,7 +65,7 @@ export function GenerateForm() {
       title: "You submitted the following values:",
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+          <code className="text-white">{JSON.stringify(values)}</code>
         </pre>
       ),
     })
